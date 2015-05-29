ea26b0dbad464bfdab87bb4150a14a5be61846d8
Mike <mcsyko@gmail.com>
2015-05-29 09:45:36 +0200

    unit tests

---
+++ HiddenWatermark.Tests/WatermarkTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HiddenWatermark.Tests
+{
+    [TestClass]
+    public class WatermarkTests
+    {
+        [TestMethod]
+        public void Watermark_Retrieve()
+        {
+            Watermark watermark = new Watermark(GetWatermarkBytes());
+            var markedImage = watermark.EmbedWatermark(GetImageBytes());
+
+            Assert.IsTrue(watermark.RetrieveWatermark(markedImage).WatermarkDetected);
+        }
+
+        [TestMethod]
+        public void Watermark_Retrieve_False()
+        {
+            Watermark watermark = new Watermark(GetWatermarkBytes());
+            Assert.IsFalse(watermark.RetrieveWatermark(GetImageBytes()).WatermarkDetected);
+        }
+
+        [TestMethod]
+        public void Watermark_RetrieveAndEmbed()
+        {
+            Watermark watermark = new Watermark(GetWatermarkBytes());
+            var markedImage = watermark.EmbedWatermark(GetImageBytes());
+
+            var result = watermark.RetrieveAndEmbedWatermark(markedImage);
+
+            Assert.IsTrue(result.WatermarkDetected);
+            Assert.IsNotNull(result.WatermarkedImage);
+        }
+
+        [TestMethod]
+        public void Watermark_RetrieveAndEmbed_False()
+        {
+            Watermark watermark = new Watermark(GetWatermarkBytes());
+            var result = watermark.RetrieveAndEmbedWatermark(GetImageBytes());
+
+            Assert.IsFalse(result.WatermarkDetected);
+            Assert.IsNotNull(result.WatermarkedImage);
+        }
+
+        private byte[] GetImageBytes()
+        {
+            return GetResourceBytes("HiddenWatermark.Tests.original.jpg");
+        }
+
+        private byte[] GetWatermarkBytes()
+        {
+            return GetResourceBytes("HiddenWatermark.Tests.watermark.jpg");
+        }
+
+        private byte[] GetResourceBytes(string resourceName)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
